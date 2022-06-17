ca61a8fcb3004cad4997f238daeefaa4a5f80433
Anthony Duong <83148834+khangbeo@users.noreply.github.com>
2022-06-16 18:21:51 -0700

    first commit

---
+++ caesar.rb
@@ -0,0 +1,13 @@
+def caesar_cipher(string, shift = 1)
+  alphabet  = Array('a'..'z')
+  non_caps  = Hash[alphabet.zip(alphabet.rotate(shift))]
+  
+  alphabet = Array('A'..'Z')
+  caps     = Hash[alphabet.zip(alphabet.rotate(shift))]
+  
+  encrypter = non_caps.merge(caps)
+  
+  string.chars.map { |c| encrypter.fetch(c, c) }.join
+end
+
+p caesar_cipher("What a string!", 5)
